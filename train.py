d12526b671e3141d35b88515a1ed29a1c1ec2290
vamsi-krishna-2005 <126351054+vamsi-krishna-2005@users.noreply.github.com>
2026-02-05 13:00:38 +0530

    Fixed Bugs

--- train.py
+++ train.py
@@ -22,8 +22,8 @@ device = torch.device("cuda" if use_cuda else "cpu")
 RUN_TAG = f"{args.model}_{'gpu' if use_cuda else 'cpu'}"
 CSV_PATH = f"results/Albumented_{RUN_TAG}_metrics.csv"
 
-TRAIN_DIR = 'data/train'
-VAL_DIR = 'data/valid'
+TRAIN_DIR = "/home/jayadeepj/Desktop/Urbanlens/data/train"
+VAL_DIR = "/home/jayadeepj/Desktop/Urbanlens/data/valid"
 
 EPOCHS = 30
 BATCH_SIZE = 2
